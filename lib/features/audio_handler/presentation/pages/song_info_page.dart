20383f82cfd829601f823c39888ff7f72b79e28a
omkar273 <omkarsonawane622@gmail.com>
2023-12-30 02:27:08 +0530

    minor ui bug fixes

--- lib/features/audio_handler/presentation/pages/song_info_page.dart
+++ lib/features/audio_handler/presentation/pages/song_info_page.dart
@@ -35,7 +35,9 @@ class _SongInfoPageState extends State<SongInfoPage>
 
   @override
   void dispose() {
+    _animationController.stop();
     _animationController.dispose();
+
     super.dispose();
   }
 
@@ -60,96 +62,100 @@ class _SongInfoPageState extends State<SongInfoPage>
 
             final song = state.songList.elementAt(state.currentPlayingIndex);
             return SafeArea(
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                width: double.maxFinite,
-                child: Column(
-                  children: [
-                    Vspace(10),
-                    RotationTransition(
-                      turns: _animation,
-                      child: const CircleAvatar(
-                        radius: 120,
-                        child: Icon(Icons.music_note, size: 100),
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  width: double.maxFinite,
+                  child: Column(
+                    children: [
+                      Vspace(10),
+                      RotationTransition(
+                        turns: _animation,
+                        child: const CircleAvatar(
+                          radius: 120,
+                          child: Icon(Icons.music_note, size: 100),
+                        ),
                       ),
-                    ),
-                    Vspace(25),
-                    Text(
-                      song.title,
-                      overflow: TextOverflow.fade,
-                      maxLines: 1,
-                      softWrap: true,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      song.artist ?? "",
-                      overflow: TextOverflow.fade,
-                      maxLines: 1,
-                      softWrap: true,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        fontSize: 20,
+                      Vspace(25),
+                      Text(
+                        song.title,
+                        overflow: TextOverflow.fade,
+                        maxLines: 1,
+                        softWrap: true,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
                       ),
-                    ),
-                    Vspace(20),
-                    Row(
-                      children: [
-                        Text(_position.toString().split(".")[0]),
-                        Expanded(
-                          child: Slider(
-                            min: const Duration(microseconds: 0)
-                                .inSeconds
-                                .toDouble(),
-                            value: _position.inMicroseconds.toDouble(),
-                            max: _duration.inMicroseconds.toDouble(),
-                            onChanged: (microSeconds) {
-                              state.seek(microSeconds.toInt());
-                            },
+                      Text(
+                        song.artist ?? "",
+                        overflow: TextOverflow.fade,
+                        maxLines: 1,
+                        softWrap: true,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      Vspace(20),
+                      Row(
+                        children: [
+                          Text(_position.toString().split(".")[0]),
+                          Expanded(
+                            child: Slider(
+                              min: const Duration(microseconds: 0)
+                                  .inSeconds
+                                  .toDouble(),
+                              value: _position.inMicroseconds.toDouble(),
+                              max: _duration.inMicroseconds.toDouble(),
+                              onChanged: (microSeconds) {
+                                state.seek(microSeconds.toInt());
+                              },
+                            ),
                           ),
-                        ),
-                        Text(_duration.toString().split(".")[0]),
-                      ],
-                    ),
-                    Vspace(25),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            context
-                                .read<MusicHandlerCubit>()
-                                .seekToPrevious(state);
-                          },
-                          icon: const Icon(Icons.skip_previous, size: 40),
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            if (state.isPlaying) {
-                              _animationController.reset();
-                              state.stopAudio();
-                            } else {
-                              _animationController.repeat();
-                              state.playAudio();
-                            }
-                            setState(() {
-                              isPlaying = state.isPlaying;
-                            });
-                          },
-                          icon: isPlaying
-                              ? const Icon(Icons.pause, size: 40)
-                              : const Icon(Icons.play_arrow_sharp, size: 40),
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            context.read<MusicHandlerCubit>().seekToNext(state);
-                          },
-                          icon: const Icon(Icons.skip_next, size: 40),
-                        ),
-                      ],
-                    )
-                  ],
+                          Text(_duration.toString().split(".")[0]),
+                        ],
+                      ),
+                      Vspace(25),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              context
+                                  .read<MusicHandlerCubit>()
+                                  .seekToPrevious(state);
+                            },
+                            icon: const Icon(Icons.skip_previous, size: 40),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              if (state.isPlaying) {
+                                _animationController.reset();
+                                state.stopAudio();
+                              } else {
+                                _animationController.repeat();
+                                state.playAudio();
+                              }
+                              setState(() {
+                                isPlaying = state.isPlaying;
+                              });
+                            },
+                            icon: isPlaying
+                                ? const Icon(Icons.pause, size: 40)
+                                : const Icon(Icons.play_arrow_sharp, size: 40),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              context
+                                  .read<MusicHandlerCubit>()
+                                  .seekToNext(state);
+                            },
+                            icon: const Icon(Icons.skip_next, size: 40),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             );
