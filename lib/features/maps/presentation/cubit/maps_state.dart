15c8138eca8ddfbb7f91200af670877e8846015f
omkar273 <omkarsonawane622@gmail.com>
2023-12-30 02:59:55 +0530

    starting map navigation

---
+++ lib/features/maps/presentation/cubit/maps_state.dart
@@ -0,0 +1,10 @@
+part of 'maps_cubit.dart';
+
+abstract class MapsState extends Equatable {
+  const MapsState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class MapsInitial extends MapsState {}
